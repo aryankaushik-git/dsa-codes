ea88e98abad40bbaddd55d45b7efc19a0695ad0b
Aryan Kaushik <aryankaushik666@gmail.com>
2022-01-05 23:41:19 +0530

    more progs.

---
+++ lecture-002/fnc_ReverseOfNo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class fnc_ReverseOfNo {
+
+    public static int reverseOfaNumber(int num){
+        int ans = 0;
+        while(num !=0)
+        {
+            int digit = num%10;
+            num /= 10;
+            ans = ans * 10 + digit;
+        }
+        return ans;
+    }
+
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int a = scn.nextInt();
+
+        int ans = reverseOfaNumber(a);
+
+        System.out.println(ans);
+    }
+    
+}
