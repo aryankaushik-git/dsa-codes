10579e46aa602bc1a274c701ef63aa16be170f52
Aryan Kaushik <aryankaushik666@gmail.com>
2022-02-09 17:39:07 +0530

    lcm_gcd

--- lecture-004/lcm_gcd.java
+++ lecture-004/lcm_gcd.java
@@ -8,21 +8,28 @@ public class lcm_gcd {
         int num1 = scn.nextInt();
         int num2 = scn.nextInt();
         scn.close();
+        gcd_lcm(num1, num2);
         
-        int gcd = 0, lcm, count = 1, small;
-        small = (num1 < num2) ? num1 : num2;
+    }
 
-        while (count <= small) {
-            if (num1 % count == 0 && num2 % count == 0) {
-                gcd = count;
-            }
-            count++;
+        public static void gcd_lcm(int n1, int n2){
+        int on1 = n1;
+        int on2 = n2;
+
+        while(n1%n2 != 0){
+            int re =n1%n2;
+            n1 =n2;
+            n2 =re;
         }
 
-        lcm = (num1 * num2) / gcd;
+        int lc = (on1 * on2) / n2 ;
 
-        System.out.println(gcd);
-        System.out.println(lcm);
+
+        System.out.println("GCD is " + n2);
+        System.out.println("lcm is " + lc);
     }
-}
+
+
+        
+    }
 
