8d0cb1c22c9ba4381df46b49728c34d740bf0c38
Aryan Kaushik <aryankaushik666@gmail.com>
2022-02-10 19:15:11 +0530

    p3

---
+++ Patterns/pattern_3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class pattern_3 {
+    public static void pattern(int n){
+        int nsp = 0;
+        int nst = n;
+        for( int i  = 1; i<= n; i++)
+        {
+            for(int csp= 1; csp<=nsp; csp++ )
+            System.out.print("\t");
+    
+            for (int cst = 1; cst<=nst; cst ++)
+            System.out.print("*\t");
+    
+          
+            nst--;
+        
+            System.out.println();
+        }
+    
+       
+    
+        }
+    
+      public static void main (String[] args){
+          Scanner scn = new Scanner(System.in);
+          int n = scn.nextInt();
+    
+          pattern(n);
+      }
+}
