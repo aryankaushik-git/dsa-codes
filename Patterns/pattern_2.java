83f50a0ccec4d48611a0c8eeaaca20f4b5f6e638
Aryan Kaushik <aryankaushik666@gmail.com>
2022-02-10 19:13:08 +0530

    p2

---
+++ Patterns/pattern_2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class pattern_2 {
+    public static void pattern(int n){
+        int nsp = 0;
+        int nst = 1;
+        for( int i  = 1; i<= n; i++)
+        {
+           
+    
+            for (int cst = 1; cst<=nst; cst ++)
+            System.out.print("*\t");
+    
+        
+            nst++;
+        
+            System.out.println();
+        }
+    
+       
+    
+        }
+    
+      public static void main (String[] args){
+          Scanner scn = new Scanner(System.in);
+          int n = scn.nextInt();
+    
+          pattern(n);
+      }
+    
+}
