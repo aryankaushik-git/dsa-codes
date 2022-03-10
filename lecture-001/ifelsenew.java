fefc808e5ba2ac5bb0ba1ef20355588f869075f2
Aryan Kaushik <aryankaushik666@gmail.com>
2022-03-10 17:05:57 +0530

    new if else

--- lecture-001/ifelsenew.java
+++ lecture-001/ifelsenew.java
@@ -3,9 +3,15 @@ public class ifelsenew {
 public static void main(String[] args) {
     
     int a=50;
-    int var=20;
+    int var=0;
 
     var = (a<10) ? 100:200;
+     // if (a<10)
+    //  {var =100}
+    // else
+    // {var= 200}
+
+
     System.out.println(var);
 }
     
